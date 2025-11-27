05b841a95d529ef0bf989e184b1a3e61d8072986
Ville-Juhani Nivasalo <villejuhani.nivasalo@icloud.com>
2025-11-27 20:22:18 +0200

    web server

--- part1/2-the-project-step-1/server.js
+++ part1/2-the-project-step-1/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 
 // Default to port 3000 if no PORT environment variable is set
+// In the Dockerfile, it is set to 3001 to demonstrate configurability
 const port = process.env.PORT || 3000;
 
 // Return a simple message for the root route
