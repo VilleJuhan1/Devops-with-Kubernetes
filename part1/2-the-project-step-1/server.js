398a491d0947673e0dc3f6f1ae7ee7aaca9b9f86
Ville-Juhani Nivasalo <villejuhani.nivasalo@icloud.com>
2025-11-27 18:54:05 +0200

    simple web app

---
+++ part1/2-the-project-step-1/server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+
+// Default to port 3000 if no PORT environment variable is set
+const port = process.env.PORT || 3000;
+
+// Return a simple message for the root route
+app.get("/", (req, res) => {
+  res.send(`Hello from port ${port}!`);
+});
+
+// Start the server and listen on the specified port
+app.listen(port, () => {
+  console.log(`Server started in port ${port}`);
+});
