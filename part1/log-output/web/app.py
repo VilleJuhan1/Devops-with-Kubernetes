eacafe0e37d695ad1659f3ecdca7263379f61ce4
Ville-Juhani Nivasalo <villejuhani.nivasalo@icloud.com>
2025-12-10 20:51:33 +0200

    logger and log reader inside same pod

--- part1/log-output/web/app.py
+++ part1/log-output/web/app.py
@@ -18,6 +18,7 @@ def logs():
                 if line_counter != 0:   # Skip the first line which is just "Started logging"
                     logs.append(line)
                 line_counter += 1
+        # The format is pretty bad at the moment, but the log format is not ready either so keep it this way for testing
         return jsonify({"100 latest log entries": logs[-100:]})
     except Exception as e:
         return jsonify({"error": str(e)}), 500
